e15bad077d4ad7f317616a8364bf7c08337f0797
Kurt <jkurt1188@gmail.com>
2023-08-08 13:25:42 +0800

    modified post-install

--- post-install.sh
+++ post-install.sh
@@ -89,8 +89,8 @@ echo 0 > /sys/class/vtconsole/vtcon1/bind
 # Unbind EFI Framebuffer
 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# Stop script for 2 seconds
-sleep 2
+# Stop script for 1 seconds
+sleep 1
 
 # Unload NVIDIA kernel modules
 modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia
