cef9a9d9be79015041717148ef6f656deed46222
Kurt <jkurt1188@gmail.com>
2023-08-08 13:18:57 +0800

    gpu bios replaced

--- pre-install-1.sh
+++ pre-install-1.sh
@@ -4,7 +4,7 @@
 dnf update -y
 
 # Enable RPM Pusion free and non-free repositories
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 # Update the repository
 dnf update --refresh -y
